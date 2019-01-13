1f2c80230e4b690f414ef5a6831ad18248a2edef
Stralcops <antoinemiloux@icloud.com>
2019-01-13 02:06:52 +0100

    Ajout resumé

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,13 +1,21 @@
 <div class="row ">
+  <?php
+  $query = $this->db->get('t_resume');
+  foreach ($query->result() as $key)
+  {
+
+   ?>
   <div class="col s8 ">
     <h1 class="m-0 f-20">Dernière partie.</h1>
     <div class="card p-20 pt-10">
-      <h2 class=""></h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <h2 class="f-15 m-0"><?php echo $key->nom_resume?></h2>
+      <p><?php echo $key->text_resume?></p>
       <br>
-      <span>Date :12/01/2019, il y a 2 mois </span>
+      <span class="w-100 flex right">Date : <?php echo $key->date_resume ?> </span>
     </div>
    </div>
+   <?php }
+    ?>
    <div class="col s2 offset-s1 ">
      <h1 class="m-0 f-15">Pjs connectés.</h1>
      <div class="card p-20 pt-10">
--- application/views/pages/personnages.php
+++ application/views/pages/personnages.php
@@ -1,12 +1,7 @@
 <table id="example" class="display" style="width:100%">
         <thead>
             <tr>
-                <th>Joueur</th>
-                <th>Personnage</th>
-                <th>Jet</th>
-                <th>Vie</th>
-                <th>Psy</th>
-                <th>Etat</th>
+
             </tr>
         </thead>
         <tbody>
@@ -21,3 +16,56 @@
 
         </tfoot>
     </table>
+    <table id="example3" class="display dataTable" style="width:100%">
+            <thead>
+                <tr>
+                  <th>Joueur</th>
+                  <th>Personnage</th>
+                  <th>Jet</th>
+                  <th>Vie</th>
+                  <th>Psy</th>
+                  <th>Etat</th>
+                </tr>
+            </thead>
+            <tbody>
+              <?php
+              $query = $this->db->get('t_armure');
+              foreach ($query->result() as $row)
+              {
+           ?>
+                <tr>
+                    <td> <?php echo $row->nom_armure ?></td>
+                    <td> <?php
+                    if($row->protec_armure == NULL OR $row->protec_armure == 0 )
+                    {
+                      echo "0";
+                    }
+                    else{
+                        echo $row->protec_armure;
+                    }
+                     ?>
+                   </td>
+                   <td> <?php
+                     if($row->rar_armure == NULL OR $row->rar_armure == 0 )
+                     {
+                       echo "0";
+                     }
+                     else{
+                         echo $row->rar_armure;
+                     } ?></td>
+                    <td><?php echo $row->cout_armure ?> Pz
+                    </td>
+                    <td>
+                      <a class='dropdown-trigger btn red darken-4 ' href='#' data-target='dropdown1'><i class="material-icons">settings</i> </a>
+                      <ul id='dropdown1' class='dropdown-content'>
+                        <li><a href="#!">Acheter</a></li>
+                        <!-- <li><a href="#!"></a></li> -->
+
+                      </ul>
+
+                    </td>
+
+                </tr>
+    <?php } ?>
+            </tfoot>
+        </table>
